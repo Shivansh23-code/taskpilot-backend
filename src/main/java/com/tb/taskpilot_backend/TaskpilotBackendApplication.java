287c8a1439ae05502ca01d7dcd9f5f7231be7796
Shivansh Tiwari <shivanshtiwari887@gmail.com>
2026-01-18 22:52:29 +0530

    Initial Spring Boot Setup

---
+++ src/main/java/com/tb/taskpilot_backend/TaskpilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tb.taskpilot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskpilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskpilotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tb/taskpilot_backend/TaskpilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tb.taskpilot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskpilotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
